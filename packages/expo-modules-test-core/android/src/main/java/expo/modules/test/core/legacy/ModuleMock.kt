2b9e35a87fa036a84322494b9bc865855fe86bb3
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2026-03-29 23:39:07 +0200

    Add Android instrument tests

--- packages/expo-modules-test-core/android/src/main/java/expo/modules/test/core/legacy/ModuleMock.kt
+++ packages/expo-modules-test-core/android/src/main/java/expo/modules/test/core/legacy/ModuleMock.kt
@@ -8,11 +8,11 @@ import expo.modules.core.interfaces.services.EventEmitter
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.ModuleHolder
 import expo.modules.kotlin.modules.Module
+import expo.modules.test.core.createMockAppContext
 import io.mockk.MockK
 import io.mockk.MockKGateway
 import io.mockk.every
 import io.mockk.mockk
-import java.lang.ref.WeakReference
 import java.lang.reflect.Proxy
 import kotlin.reflect.KClass
 
@@ -120,11 +120,7 @@ data class ModuleMock<TestInterfaceType : Any, ModuleType : Module>(
 
 private fun prepareMockAppContext(customAppContext: AppContext?): AppContext {
   val reactContext = BridgeReactContext(ApplicationProvider.getApplicationContext<Context>())
-  val appContext = customAppContext ?: AppContext(
-    modulesProvider = mockk(relaxed = true),
-    legacyModuleRegistry = mockk(relaxed = true),
-    reactContextHolder = WeakReference(reactContext)
-  )
+  val appContext = customAppContext ?: createMockAppContext()
 
   // as AppContext holds only weak reference to Android Context which can be destroyed too early
   // we need to override it to return actual strong reference (held by mockk internals)
